e7aa3b40a34765c7a56e49509d5c36dd8e95e770
suraj regmi <hyakoste@gmail.com>
2021-01-19 18:43:52 +0545

    add to-do-item component and make todo app functional

--- to-do-app.js
+++ to-do-app.js
@@ -5,8 +5,11 @@ class TodoApp extends HTMLElement {
   constructor() {
     super();
     this._shadowRoot = this.attachShadow({ mode: "open" });
+
+    // render(this.template(), this._shadowRoot, { eventContext: this });
+    this.todos = [{ text: "Learn about Lit-html", checked: true }];
     this.$input = this._shadowRoot.querySelector("input");
-    render(this.template(), this._shadowRoot, { eventContext: this });
+    // make sure you maintain order...if input is declared above todos it will throw error.
   }
 
   template() {
@@ -32,9 +35,48 @@ class TodoApp extends HTMLElement {
       <h1>TODO App In Lit-html</h1>
       <form id="todo-input">
         <input type="text" placeholder="Add your Todo" />
-        <button>Add</button>
+        <button @click=${this._addTodo}>Add</button>
       </form>
+      <ul id="todos">
+        ${this.todos.map(
+          (todo, index) => html`
+            <to-do-item
+              ?checked=${todo.checked}
+              .index=${index}
+              text=${todo.text}
+              @onRemove=${this._removeTodo}
+              @onToggle=${this._toggleTodo}
+            ></to-do-item>
+          `
+        )}
+      </ul>
     `;
+  }
+
+  _addTodo(e) {
+    // preventDefault cancels the event if it is cancelable.
+    // for eg : Clicking on a "Submit" button, prevent it from submitting a form
+    e.preventDefault();
+    if (this.$input.value.length > 0) {
+      // ... spread operator used as bracket eraser ..this line is just like pushing element into array.
+      this.todos = [...this.todos, { text: this.$input.value, checked: false }];
+      this.$input.value = "";
+    }
+    console.log(this.todos);
+  }
+
+  _removeTodo(e) {
+    // index to be removed comes from custom event (e) and filter returns todos where
+    // index is not equal to index of deleted todo.
+    this.todos = this.todos.filter((todo, index) => {
+      return index !== e.detail;
+    });
+  }
+
+  _toggleTodo(e) {
+    this.todos = this.todos.map((todo, index) => {
+      return index === e.detail ? { ...todo, checked: !todo.checked } : todo;
+    });
   }
 
   set todos(value) {
---
+++ to-do-item.js
@@ -0,0 +1,93 @@
+import { html, render } from "lit-html";
+
+class TodoItem extends HTMLElement {
+  constructor() {
+    super();
+    this._shadowRoot = this.attachShadow({ mode: "open" });
+  }
+
+  template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+          font-family: sans-serif;
+        }
+        .completed {
+          text-decoration: line-through;
+        }
+        button {
+          border: none;
+          cursor: pointer;
+          background-color: Transparent;
+          color: red;
+        }
+      </style>
+      <li class="item">
+        <input
+          type="checkbox"
+          .checked=${this._checked}
+          @change=${this._fireToggle}
+        />
+        <label class=${this._checked ? "completed" : ""}>${this._text}</label>
+        <button @click=${this._fireRemove}>Remove</button>
+      </li>
+    `;
+  }
+
+  connectedCallback() {
+    if (!this.hasAttribute("text")) {
+      this.setAttribute("text", "placeholder");
+    }
+    render(this.template(), this._shadowRoot, { eventContext: this });
+  }
+
+  _fireToggle(e) {
+    this.dispatchEvent(new CustomEvent("onToggle", { detail: this.index }));
+  }
+
+  _fireRemove(e) {
+    this.dispatchEvent(new CustomEvent("onRemove", { detail: this.index }));
+  }
+
+  static get observedAttributes() {
+    return ["text", "checked", "index"];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    switch (name) {
+      case "text":
+        this._text = newValue;
+        break;
+      case "checked":
+        this._checked = this.hasAttribute("checked");
+        break;
+      case "index":
+        this._index = parseInt(newValue);
+        break;
+    }
+    render(this.template(), this._shadowRoot, { eventContext: this });
+  }
+
+  set index(val) {
+    this.setAttribute("index", val);
+  }
+
+  get index() {
+    return this._index;
+  }
+
+  get checked() {
+    return this.hasAttribute("checked");
+  }
+
+  set checked(val) {
+    if (val) {
+      this.setAttribute("checked", "");
+    } else {
+      this.removeAttribute("checked");
+    }
+  }
+}
+
+window.customElements.define("to-do-item", TodoItem);
